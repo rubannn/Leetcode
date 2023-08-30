79a68d9914b2cb28793f99bfd7ec25fc013c273e
m.ruban <m.ruban@testportal.gov.ua>
2023-08-30 15:36:04 +0200

    modified:   _0024.py

--- _0024.py
+++ _0024.py
@@ -7,4 +7,20 @@ class ListNode:
 
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        ...
+        nodes = head
+        if nodes and nodes.next:
+            head = nodes.next
+        last = ListNode(None)
+        while nodes and nodes.next:
+            node1 = nodes
+            node2 = nodes.next
+            print(node1, node2, sep='\n')
+            last = node2.next
+
+            node2.next = node1
+            node1.next = last
+            print('===')
+            print(node1, node2, sep='\n')
+            nodes = last
+        print('~~~\n', head)
+        return head
