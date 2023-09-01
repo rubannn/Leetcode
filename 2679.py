d128b5a8b318d6dc17b81563c2008ee268ee8bd5
m.ruban <m.ruban@testportal.gov.ua>
2023-09-01 15:28:34 +0200

    update sol 2679

--- 2679.py
+++ 2679.py
@@ -1,4 +1,5 @@
 class Solution:
+# variant 1
     def matrixSum(self, nums: List[List[int]]) -> int:
         s, p = 0, len(nums[0])
         for _ in range(p):
@@ -9,5 +10,19 @@ class Solution:
                 t.remove(cmx)
                 nums[x] = t
                 mx.append(cmx)
+            s += max(mx)
+        return s
+
+# variant 2
+    def matrixSum(self, nums: List[List[int]]) -> int:
+        for x in range(len(nums)):
+            nums[x] = sorted(nums[x])[::-1]
+
+        s, p, c = 0, len(nums[0]), 0
+        for _ in range(p):
+            mx = []
+            for x in range(len(nums)):
+                mx.append(nums[x][c])
+            c += 1
             s += max(mx)
         return s
