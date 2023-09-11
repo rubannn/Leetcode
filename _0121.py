ba82743e76d98ce68e3fe21bc3ef9c441208b0e9
m.ruban <m.ruban@testportal.gov.ua>
2023-09-11 11:03:28 +0200

    create folders for solutions

--- _0121.py
+++ _0121.py
@@ -1,17 +1,17 @@
 def maxProfit(prices: list[int]) -> int:
-    b = float('-inf')
-    # b = float('inf')
+    # b = float('-inf')
+    b = float('inf')
     s = 0
     for p in prices:
-        b = max(-p, b)
-        s = max(p + b, s)
-        # b = min(p, b)
-        # s = max(p - b, s)
+        # b = max(-p, b)
+        # s = max(p + b, s)
+        b = min(p, b)
+        s = max(p - b, s)
         print(b, s)
     return s
 
 
-prices = [7,1,5,3,6,4]
+prices = [7, 1, 5, 3, 6, 4]
 out = 5
 print(maxProfit(prices) == out)
 
