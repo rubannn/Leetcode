3804ae4bc50630b17905f7766e5030bf97c5f84f
m.ruban <m.ruban@testportal.gov.ua>
2023-09-21 13:38:00 +0200

    1275 update

--- 1001 - 2000/1275.py
+++ 1001 - 2000/1275.py
@@ -1,12 +1,14 @@
-def tictactoe(self, moves: list[list[int]]) -> str:
-    board = [[0 for _ in range(3)] for _ in range(3)]
-    for i, p in enumerate(moves):
-        board[p[0]][p[1]] = [1, -1][i % 2]
-    sol = [sum(x) for x in board] + [sum(x) for x in zip(*board)] + [sum(board[i][i] for i in range(3)), sum(board[i][2-i] for i in range(3))]
-    if 3 in sol:
-        return 'A'
-    elif -3 in sol:
-        return 'B'
-    elif sum(x.count(0) for x in board) > 0:
-        return 'Pending'
-    return 'Draw'
+class Solution:
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        board = [[0 for _ in range(3)] for _ in range(3)]
+        for i, p in enumerate(moves):
+            board[p[0]][p[1]] = [1, -1][i % 2]
+        sol = [sum(x) for x in board] + [sum(x) for x in zip(*board)] + \
+            [sum(board[i][i] for i in range(3)), sum(board[i][2-i] for i in range(3))]
+        if 3 in sol:
+            return 'A'
+        elif -3 in sol:
+            return 'B'
+        elif len(moves) < 9:
+            return 'Pending'
+        return 'Draw'
