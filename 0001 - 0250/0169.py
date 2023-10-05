b869e136dbb5462177620841c3c9d0cbd96f7fcb
m.ruban <m.ruban@testportal.gov.ua>
2023-10-05 08:48:59 +0200

    update 169

--- 0001 - 0250/0169.py
+++ 0001 - 0250/0169.py
@@ -1,4 +1,8 @@
+from math import floor
+from collections import Counter
+
 class Solution:
+    # variant 1
     def majorityElement(self, nums: List[int]) -> int:
         nm, mx = dict(), [nums[0], 1]
         for i in range(len(nums)):
@@ -9,3 +13,9 @@ class Solution:
             else:
                 nm[nums[i]] = 1
         return mx[0]
+
+    # variant 2
+    def majorityElement2(self, nums: List[int]) -> int:
+        dct = Counter(nums)
+        t = floor(len(nums) / 2)
+        return [x for x, y in dct.items() if y > t][0]
