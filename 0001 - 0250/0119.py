c03fb76c6255610bcf3453e66b840c438897d826
m.ruban <m.ruban@testportal.gov.ua>
2023-10-16 08:31:43 +0200

    update 119

--- 0001 - 0250/0119.py
+++ 0001 - 0250/0119.py
@@ -1,4 +1,5 @@
 class Solution:
+    # variant 1
     def getRow(self, rowIndex: int) -> List[int]:
         tr = [[1]]
         for _ in range(rowIndex):
@@ -7,3 +8,13 @@ class Solution:
                 last += [tr[-1][k] + tr[-1][k+1]]
             tr.append(last + [1])
         return tr[-1]
+
+    # variant 2
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for _ in range(rowIndex):
+            last = [1]
+            for k in range(len(row)-1):
+                last += [row[k] + row[k+1]]
+            row = last + [1]
+        return row
