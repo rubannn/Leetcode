bf1c8d45727088bd96fe1597268fe1ed0ae6c831
m.ruban <m.ruban@testportal.gov.ua>
2023-12-07 09:00:48 +0100

    update 1716

--- 1501 - 1750/1716.py
+++ 1501 - 1750/1716.py
@@ -1,4 +1,5 @@
 class Solution:
+    # variant 1
     def totalMoney(self, n: int) -> int:
         p, s = 1, 0
         while n > 7:
@@ -7,3 +8,21 @@ class Solution:
             p += 1
         s += (2 * p + n - 1) * n // 2
         return s
+
+    # variant 2
+    def totalMoney2(self, n: int) -> int:
+        res = 0
+        a, b = divmod(n, 7)
+        for i in range(1, a + 1):
+            res += i + 3
+        res = res * 7 + (2 * a + b + 1) * b // 2
+        return res
+
+
+sol = Solution()
+tests = [
+    (4, 10), (10, 37), (20, 96)
+]
+
+for n, ans in tests:
+    print(sol.totalMoney(n) == ans)
