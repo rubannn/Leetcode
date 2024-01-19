887690e6f12cff8a411a024755c5f3d693eb453b
m.ruban <m.ruban@testportal.gov.ua>
2024-01-19 08:46:42 +0100

    task 0931

--- 0751 - 1000/0931.py
+++ 0751 - 1000/0931.py
@@ -1,4 +1,8 @@
+from typing import List
+
+
 class Solution:
+    # variant 1
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
         for i in range(n - 1, 0, -1):
@@ -9,4 +13,14 @@ class Solution:
                 if j + 1 < n:
                     mn = min(matrix[i][j + 1], mn)
                 matrix[i - 1][j] += mn
+        return min(matrix[0])
+
+    # variant 2
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
+        for i in range(n - 1, 0, -1):
+            for j in range(n):
+                matrix[i - 1][j] += min(
+                    matrix[i][j], matrix[i][max(j - 1, 0)], matrix[i][min(j + 1, n - 1)]
+                )
         return min(matrix[0])
