54b7fe25f4099869fc482c30e9f2312576df5a27
m.ruban <m.ruban@testportal.gov.ua>
2024-01-22 08:39:58 +0100

    update 0645

--- 0501 - 0750/0645.py
+++ 0501 - 0750/0645.py
@@ -1,4 +1,15 @@
-def findErrorNums(self, nums: list[int]) -> list[int]:
-    for x in nums:
-        if nums.count(x) == 2:
-            return [x, len(nums)*(len(nums) + 1) // 2 - sum(nums) + x]
+class Solution:
+    # variant 1 (9632 ms 15.4 MB)
+    def findErrorNums(self, nums: list[int]) -> list[int]:
+        for x in nums:
+            if nums.count(x) == 2:
+                return [x, len(nums) * (len(nums) + 1) // 2 - sum(nums) + x]
+
+    # variant 2 (157 ms 18.6 MB)
+    def findErrorNums(self, nums: list[int]) -> list[int]:
+        nums_set = set()
+        for x in nums:
+            if x not in nums_set:
+                nums_set.add(x)
+            else:
+                return [x, len(nums) * (len(nums) + 1) // 2 - sum(nums) + x]
