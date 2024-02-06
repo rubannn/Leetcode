bbd7e4e5d8c3ed32dd72a36fdb878ed2ab6150bc
m.ruban <m.ruban@testportal.gov.ua>
2024-02-06 09:11:08 +0100

    update task 0049

--- 0001 - 0250/0049.py
+++ 0001 - 0250/0049.py
@@ -1,9 +1,21 @@
-def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+from typing import List
+from collections import defaultdict
+
+
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
     res = dict()
     for x in strs:
-        v = ''.join(sorted(x))
+        v = "".join(sorted(x))
         if res.get(v, None) is None:
             res[v] = [x]
         else:
             res[v] += [x]
     return res.values()
+
+
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    dc = defaultdict(list)
+    for s in strs:
+        key = "".join(sorted(s))
+        dc[key].append(s)
+    return dc.values()
