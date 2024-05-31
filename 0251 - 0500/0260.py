8f0d4eb3547c5d4f615242e978e46163acfe3c0a
m.ruban <m.ruban@testportal.gov.ua>
2024-05-31 09:17:57 +0200

    add new sol to task 0260

--- 0251 - 0500/0260.py
+++ 0251 - 0500/0260.py
@@ -1,3 +1,20 @@
 class Solution:
+    # variant 1
     def singleNumber(self, nums: List[int]) -> List[int]:
         return [x for x in set(nums) if nums.count(x) == 1]
+
+    # variant 2
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        nums += ["~"]
+        res = []
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                i += 2
+            else:
+                res.append(nums[i])
+                i += 1
+                if len(res) == 2:
+                    return res
+        return res
