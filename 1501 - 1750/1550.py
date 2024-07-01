440d8f38e931aeb0792b1418573849fea7dd11b8
m.ruban <m.ruban@testportal.gov.ua>
2024-07-01 12:40:09 +0200

    task 1550

--- 1501 - 1750/1550.py
+++ 1501 - 1750/1550.py
@@ -1,6 +1,19 @@
 class Solution:
+    # variant 1
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
         for i in range(len(arr) - 2):
             if arr[i] % 2 == arr[i + 1] % 2 == arr[i + 2] % 2 == 1:
                 return True
+        return False
+
+    # variant 2
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odd = 0
+        for x in arr:
+            if x % 2 == 1:
+                odd += 1
+                if odd == 3:
+                    return True
+            else:
+                odd = 0
         return False
