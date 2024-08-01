375ca9d35ad9e4a3527183e5fbb1883fd69169f8
m.ruban <m.ruban@testportal.gov.ua>
2024-08-01 09:28:03 +0200

    upd 1980

--- 1751 - 2000/1980.py
+++ 1751 - 2000/1980.py
@@ -1,8 +1,7 @@
 # https://en.wikipedia.org/wiki/Cantor%27s_diagonal_argument
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        ans, p = '', 0
-        for x in nums:
-            ans += '01'[x[p] == '0']
-            p += 1
+        ans = ''
+        for i, x in enumerate(nums):
+            ans += '01'[x[i] == '0']
         return ans
