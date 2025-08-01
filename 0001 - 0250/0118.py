48bf1e8ef21186c36263159cd3dd400dfa3bf549
m.ruban <m.ruban@testportal.gov.ua>
2025-08-01 08:44:14 +0200

    update task 0118

--- 0001 - 0250/0118.py
+++ 0001 - 0250/0118.py
@@ -1,9 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        tr = [[1]]
-        for _ in range(numRows-1):
+        res = [[1]]
+        for _ in range(numRows - 1):
             last = [1]
-            for k in range(len(tr[-1])-1):
-                last += [tr[-1][k] + tr[-1][k+1]]
-            tr.append(last + [1])
-        return tr
+            for k in range(len(res[-1]) - 1):
+                last += [res[-1][k] + res[-1][k + 1]]
+            res.append(last + [1])
+        return res
